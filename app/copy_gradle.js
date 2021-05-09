43eada608e5d7a4c5897dc39c165a71890c67734
Tarsis de Lima <br.tmvdl@gmail.com>
2021-05-09 00:37:44 -0300

    Fixing the hooks

--- app/copy_gradle.js
+++ app/copy_gradle.js
@@ -1,4 +1,27 @@
-// cp ./res/gradle.properties ./platforms/android/app/gradle.properties -f
-const from = './res/gradle.properties'
-const to = './platforms/android/app/gradle.properties'
-require('fs').copyFileSync(from , to)
+const path = require('path')
+const fs = require('fs')
+
+const copy = path.resolve(__dirname, './platforms/android/gradle.properties')
+
+console.log('copy function', { copy })
+
+console.log('before try')
+
+try {
+  const text = fs.readFileSync(copy, 'utf8')
+
+  console.log('readed', { copy, text })
+
+  const replaced = text.replace(/[=]false/ig, '=true')
+
+  fs.unlinkSync(copy)
+
+  fs.writeFileSync(copy, replaced, 'utf8')
+
+  console.log('copied', { copy, text, replaced })
+
+} catch (err) {
+  console.error({ err })
+}
+
+console.log('after try')
---
+++ app/copy_gradle.sh
@@ -0,0 +1,34 @@
+
+mkdir -p ./platforms/android/
+
+echo 'android.useAndroidX=true' >> ./platforms/android/gradle.properties
+
+echo 'android.enableJetifier=true' >> ./platforms/android/gradle.properties
+
+# const path = require('path')
+# const fs = require('fs')
+
+# const copy = path.resolve(__dirname, './platforms/android/gradle.properties')
+
+# console.log('copy function', { copy })
+
+# console.log('before try')
+
+# try {
+#   const text = fs.readFileSync(copy, 'utf8')
+
+#   console.log('readed', { copy, text })
+
+#   const replaced = text.replace(/[=]false/ig, '=true')
+
+#   fs.unlinkSync(copy)
+
+#   fs.writeFileSync(copy, replaced, 'utf8')
+
+#   console.log('copied', { copy, text, replaced })
+
+# } catch (err) {
+#   console.error({ err })
+# }
+
+# console.log('after try')
