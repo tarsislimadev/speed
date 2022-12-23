05ab2753adad1ec05ba52c9fac7938444fb9e95a
Tarsis <br.tmvdl@gmail.com>
2022-12-23 20:52:21 -0300

    Fixing scripts

--- env/build.sh
+++ env/build.sh
@@ -1,7 +1,7 @@
-docker-compose -f ./docker-compose.yaml run app ionic cordova platform remove android --nosave
+docker-compose -f ./docker-compose.yaml run app ionic cordova platform remove android --nosave --prod --no-interactive 
  
-docker-compose -f ./docker-compose.yaml run app ionic cordova platform add android --confirm --no-interactive --noresources 
+docker-compose -f ./docker-compose.yaml run app ionic cordova platform add android --confirm --prod --no-interactive --noresources 
 
-docker-compose -f ./docker-compose.yaml run app ionic cordova build android --release --prod --no-interactive --device
+docker-compose -f ./docker-compose.yaml run app ionic cordova build android --release --prod --no-interactive --device 
 
-# docker-compose -f ./docker-compose.yaml run app jarsigner -sigalg SHA1withRSA -digestalg SHA1 -keystore $CM_KEYSTORE_PATH -storepass $CM_KEYSTORE_PASSWORD -keypass $CM_KEY_PASSWORD $(find platforms/android/app/build/outputs/apk/release -name "*.apk" | head -1) $CM_KEY_ALIAS 
+docker-compose -f ./docker-compose.yaml run app jarsigner -sigalg SHA1withRSA -digestalg SHA1 -keystore $CM_KEYSTORE_PATH -storepass $CM_KEYSTORE_PASSWORD -keypass $CM_KEY_PASSWORD /app/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $CM_KEY_ALIAS 
