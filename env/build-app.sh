996432654a7f30568518f384ea95d3f40a012dca
Tarsis <br.tmvdl@gmail.com>
2022-12-27 12:00:56 -0300

    Fixing BUILD APP script

--- env/build-app.sh
+++ env/build-app.sh
@@ -4,4 +4,4 @@ docker-compose -f ./docker-compose.yaml run app ionic cordova platform add andro
 
 docker-compose -f ./docker-compose.yaml run app ionic cordova build android --release --prod --no-interactive --device 
 
-docker-compose -f ./docker-compose.yaml run app jarsigner -sigalg SHA1withRSA -digestalg SHA1 -keystore $CM_KEYSTORE_PATH -storepass $CM_KEYSTORE_PASSWORD -keypass $CM_KEY_PASSWORD /app/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $CM_KEY_ALIAS 
+# docker-compose -f ./docker-compose.yaml run app jarsigner -sigalg SHA1withRSA -digestalg SHA1 -keystore $CM_KEYSTORE_PATH -storepass $CM_KEYSTORE_PASSWORD -keypass $CM_KEY_PASSWORD /app/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $CM_KEY_ALIAS 
